36e36d61c7a152d072e859cd3f0606f2163d6cdf
wedemeda <gagarin.rama@gmail.com>
2025-03-31 14:40:12 +0200

    Add ip_sender

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import asyncio
 import logging
 import os
+import subprocess
 
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, F
 from aiogram.filters import Command
 from aiogram.types import Message
 
@@ -18,10 +19,21 @@ bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
 
 
+def aus_ip_getter():
+    result = subprocess.run(["wget", "-qO-", "eth0.me"], stdout=subprocess.PIPE, text=True)
+    return result.stdout.strip()
+
+
 @dp.message(Command('start'))
 async def start(message: Message):
     if message.chat.id in ALLOWED_USERS:
-        await message.answer('Привет!')
+        await message.answer(f'Привет, {message.from_user.first_name}!')
+
+
+@dp.message(F.text == 'Узнать текущий ip сервера')
+async def ip_sender(message: Message):
+    if message.chat.id in ALLOWED_USERS:
+        await message.answer(f'Текущий ip-адрес сервера:: {aus_ip_getter()}')
 
 
 # Запуск бота
